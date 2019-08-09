1dd8cdfcf26805689bd3dd6bda65f5fe0fb44d18
sainalshah <sainaledava@gmail.com>
2019-08-09 18:13:02 +0800

    update prod api url and push new build

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL of development API
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://ec2-18-139-225-117.ap-southeast-1.compute.amazonaws.com:3000'
 };
